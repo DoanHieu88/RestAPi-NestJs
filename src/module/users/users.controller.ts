96d15edc4be712cf749b4afc0e76f7784a04caad
DoanHieu <hieuhthor@gmail.com>
2022-03-17 16:36:48 +0700

    fix api delete

--- src/module/users/users.controller.ts
+++ src/module/users/users.controller.ts
@@ -1,7 +1,17 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 import { CreateUserDTO } from './users.dto';
 import { UserService } from './users.service';
 
+@ApiTags('User')
 @Controller('user')
 export class UserController {
   constructor(private userService: UserService) {}
@@ -14,5 +24,31 @@ export class UserController {
   @Post('creat')
   public async createUser(@Body() body: CreateUserDTO) {
     return await this.userService.createUser(body);
+  }
+
+  @Put('update/:id')
+  public async updateUser(
+    @Param('id') id: string,
+    @Body() body: CreateUserDTO,
+  ) {
+    try {
+      return await this.userService.updateUser(id, body);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  @Delete('delete/:id')
+  public async deleteUser(@Param('id') id: string) {
+    try {
+      return await this.userService.deleteUser(id);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  @Get('get/:id')
+  public async getUserByID(@Param('id') id: string) {
+    return await this.userService.getUserById(id);
   }
 }
--- src/module/users/users.service.ts
+++ src/module/users/users.service.ts
@@ -24,4 +24,35 @@ export class UserService {
       throw new Error(error);
     }
   }
+
+  public async updateUser(id: string, body: CreateUserDTO) {
+    const userFind = await this.userRepository.findOne(id);
+    if (!userFind) {
+      throw new Error("Can't find user");
+    } else {
+      const userUpdate = await this.userRepository.save({
+        ...userFind,
+        ...body,
+      });
+      return userUpdate;
+    }
+  }
+
+  public async deleteUser(id: string) {
+    const userFind = await this.userRepository.findOneOrFail(id);
+    if (!userFind) {
+      throw new Error('can not find user');
+    } else {
+      return await this.userRepository.remove(userFind);
+    }
+  }
+
+  public async getUserById(id: string) {
+    const userFind = await this.userRepository.findOneOrFail(id);
+    if (!userFind) {
+      throw new Error('can not find user');
+    } else {
+      return await this.userRepository.findOne(userFind);
+    }
+  }
 }
